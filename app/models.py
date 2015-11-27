a4ac0fc8fdd91f9079191d51e6998d0fd5e86414
liuge1994 <610562301@qq.com>
2015-11-27 22:28:15 +0800

    add mail id and username

--- app/models.py
+++ app/models.py
@@ -1,8 +1,15 @@
+from flask.ext.login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(db.Model):
 
+    __tablename__ = 'users'
+    id = db.Column(db.Interger, primary_key = True)
+    email = db.Column(db.String(64), unique=True, index=True)
+    username = db.Column(db.String(64), unique=True, index=True)
     password_hash = db.Column(db.String(128))
+    role_id = db.Column(db.Interger, db.ForeignKey('roles.id'))
+
 
     @property 
     def password(self):
