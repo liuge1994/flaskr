f0e65641a9c01cc10330e5f80476fc385a8e90e5
liuge1994 <610562301@qq.com>
2015-11-27 20:56:40 +0800

    using Application Factory

---
+++ app/__init__.py
@@ -0,0 +1,6 @@
+from flask import Flask, render_template
+from flask.ext.bootstrap import Bootstrap 
+from flask.ext.mail import Mail 
+from flask.ext.moment import Moment 
+from flask.ext.sqlalchemy import SQLAlchemy 
+from config import config
