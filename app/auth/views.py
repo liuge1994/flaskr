4283883a9670794ec5c1e9279be0057c79a2da77
liuge1994 <610562301@qq.com>
2015-11-28 13:27:13 +0800

    submit url

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,6 +1,9 @@
-from flask import render_template
+from flask import render_template, reditect, request, url_for, flash
+from flask.ext.login import login_required, login_user
 from . import auth
-from flask.ext.login import login_required
+from ..models import User 
+from .forms import LoginForm
+
 
 @app.route('/secret')
 @login_required
@@ -10,3 +13,14 @@ def secret():
 @auth.route('/login')
 def login():
     return render_template('auth/login.html')
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user is not None and user.verify_password(form.password.data):
+            login_user(user, form.remember_me.data)
+            return reditect(request.args.get('next') or url_for('main.index'))
+        flash('Invalid username or password')
+    return render_template('auth/login.html', form=form)
