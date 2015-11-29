9bb609d8656f09c6399e43b33e1c17a422cd7385
liuge1994 <610562301@qq.com>
2015-11-29 11:26:42 +0800

    add user profile

---
+++ app/main/views.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+from flask import render_template, session, redirect, url_for
+
+from . import main
+from .forms import NameForm
+from .. import db
+from ..models import User
+
+@main.route('/', methods=['GET', 'POST'])
+def index():
+    form = NameForm()
+    if form.validate_on_submit():
+        #
+        return redirect(url_for('.index'))
+    return render_template('index.html', form=form, name=session.get('name'),
+                           known=session.get('known', False),
+                           current_time=datetime.utcnow())
+@main.route('/user/<username>')
+def user(username):
+    user = User.query.filter_by(username=username).first()
+    if user is None:
+        abort(404)
+    return render_template('user.html', user=user)
